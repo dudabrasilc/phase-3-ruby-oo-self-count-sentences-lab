74013c2f84bfaaa36b6ceadc16aa5f3813e820da
Maria Coitinho <brasildu@gmail.com>
2022-11-03 01:50:19 -0500

    commit tree ecd8e09f24bbae8fa13c7d80dae07d6e34111053

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # binding.pry
+    self.split(/[.?!...]/).length
   end
+
 end
+
+# new_string = "maria."
+
+# "hi! How? Are, You.".count_sentences
+
+# binding.pry
